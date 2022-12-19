9da2467e95bc8d298db9d7b70914f563bdeb9b89
sdelpupp <120644185+sdelpupp@users.noreply.github.com>
2022-12-19 09:38:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.text(my_data_row)
 
 
 add_my_fruit = streamlit.text_input("Add another fruit:")
-my_cur.execute("INSERT INTO fruit_load_list VALUES (" + add_my_fruit + ");")
+my_cur.execute("INSERT INTO fruit_load_list VALUES ('from streamlit');")
 
 
 
